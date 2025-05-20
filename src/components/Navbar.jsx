ef27eae3d90e12cf4e979bcf7d6f29aa4b96d0fb
Rafael Agredano <rafaelagredano99@gmail.com>
2025-05-20 15:36:00 -0500

    spaced out the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
 import "../styles/layout.css";
 
+
 export default function Navbar() {
   return (
-    <nav className="bg-yellow-300 p-4 flex justify-center items-center shadow-md">
-      <div className="space-x-20 text-lg">
-        <Link to="/" className="hover:text-blue-600">Home</Link>
-        <Link to="/toys" className="hover:text-blue-600">Toys</Link>
-        <Link to="/about" className="hover:text-blue-600">About</Link>
-        <Link to="/blog" className="hover:text-blue-600">Blog</Link>
+    <nav className="bg-yellow-300 p-4 shadow-md">
+      <div className="flex justify-center items-center space-x-16 text-lg">
+        <Link to="/" className="inline-block hover:text-blue-600">Home</Link>
+
+        <Link to="/toys" className="inline-block hover:text-blue-600">Toys</Link>
+
+        <Link to="/about" className="inline-block hover:text-blue-600">About</Link>
+        
+        <Link to="/blog" className="inline-block hover:text-blue-600">Blog</Link>
       </div>
     </nav>
   );
