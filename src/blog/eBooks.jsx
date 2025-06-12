533902cd4fced47c864f3d510fc9f94043a4c030
Rafael Agredano <rafaelagredano99@gmail.com>
2025-06-12 15:51:23 -0500

    slight fix for appearance

--- src/blog/eBooks.jsx
+++ src/blog/eBooks.jsx
@@ -193,8 +193,8 @@ const EBooks = () => {
           <div key={index} className="border rounded-lg p-4 shadow hover:shadow-lg transition">
             <img src={product.image} alt={product.title} className="w-full h-48 object-cover mb-4 rounded" />
             <h2 className="text-xl font-semibold mb-2">{product.title}</h2>
-            <p className="text-gray-700 mb-2"><strong><bold>Description:   </bold></strong>{product.description}</p>
-            <p className="text-green-500 font-bold mb-2"><strong><bold>Review:   </bold></strong>{product.text}</p>
+            <p className="text-gray-700 mb-2"><strong>Description:   </strong>{product.description}</p>
+            <p className="text-green-500 font-bold mb-2"><strong>Review:   </strong>{product.text}</p>
             <p className="text-green-600 font-bold mb-4">{product.price}</p>
             <a
               href={product.link}
